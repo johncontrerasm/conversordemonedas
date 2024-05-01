8f699774f11de529fcaba9f5a39418958fb1913d
John Contreras <john.contreras.medina@gmail.com>
2024-04-30 22:25:24 -0500

    adding the lasts conversions

--- src/com/aluracursos/principal/Principal.java
+++ src/com/aluracursos/principal/Principal.java
@@ -38,18 +38,7 @@ public class Principal {
             switch(opcion){
                 case 1:
                     System.out.println("Opcion 1");
-                    System.out.println("Ingrese el valor a convertir a USD");
-                    valoraconvertir = teclado.nextDouble();
-                    currency="ARS";
-                    modenasapi = consulta.consultarAPI(currency);
-                    moneda = new Moneda(currency,Double.valueOf(modenasapi.conversion_rates().get(currency)));
-                    System.out.println("Conversion a Dolares "+moneda.ConvertirAUSD(valoraconvertir,modenasapi.conversion_rates().get(currency)));
-                    valorconvertido = moneda.ConvertirAUSD(valoraconvertir,moneda.ConvertirAOTRA( valoraconvertir, modenasapi.conversion_rates().get(currency)));
-                    moneda.messagetousd(valoraconvertir,currency,valorconvertido);
-                    break;
-                case 2:
-                    System.out.println("Opcion 2");
-                    System.out.println("Ingrese el valor a convertir a USD");
+                    System.out.println("Ingrese el valor a convertir a ARS");
                     valoraconvertir = teclado.nextDouble();
                     currency="ARS";
                     modenasapi = consulta.consultarAPI(currency);
@@ -58,17 +47,59 @@ public class Principal {
                     moneda.messagetootra(valoraconvertir,currency,valorconvertido);
 
                     break;
+                case 2:
+                    System.out.println("Opcion 2");
+                    System.out.println("Ingrese el valor a convertir a USD");
+                    valoraconvertir = teclado.nextDouble();
+                    currency="ARS";
+                    modenasapi = consulta.consultarAPI(currency);
+                    moneda = new Moneda(currency,Double.valueOf(modenasapi.conversion_rates().get(currency)));
+                    System.out.println("Conversion a Dolares "+moneda.ConvertirAUSD(valoraconvertir,modenasapi.conversion_rates().get(currency)));
+                    valorconvertido = moneda.ConvertirAUSD(valoraconvertir,moneda.ConvertirAOTRA( valoraconvertir, modenasapi.conversion_rates().get(currency)));
+                    moneda.messagetousd(valoraconvertir,currency,valorconvertido);
+
+                    break;
                 case 3:
                     System.out.println("Opcion 3");
+                    System.out.println("Ingrese el valor a convertir a BRL");
+                    valoraconvertir = teclado.nextDouble();
+                    currency="BRL";
+                    modenasapi = consulta.consultarAPI(currency);
+                    moneda = new Moneda(currency,Double.valueOf(modenasapi.conversion_rates().get(currency)));
+                    valorconvertido = moneda.ConvertirAOTRA( valoraconvertir, moneda.getValor());
+                    moneda.messagetootra(valoraconvertir,currency,valorconvertido);
                     break;
                 case 4:
                     System.out.println("Opcion 4");
+                    System.out.println("Ingrese el valor a convertir a USD");
+                    valoraconvertir = teclado.nextDouble();
+                    currency="BRL";
+                    modenasapi = consulta.consultarAPI(currency);
+                    moneda = new Moneda(currency,Double.valueOf(modenasapi.conversion_rates().get(currency)));
+                    System.out.println("Conversion a Dolares "+moneda.ConvertirAUSD(valoraconvertir,modenasapi.conversion_rates().get(currency)));
+                    valorconvertido = moneda.ConvertirAUSD(valoraconvertir,moneda.ConvertirAOTRA( valoraconvertir, modenasapi.conversion_rates().get(currency)));
+                    moneda.messagetousd(valoraconvertir,currency,valorconvertido);
                     break;
                 case 5:
                     System.out.println("Opcion 5");
+                    System.out.println("Ingrese el valor a convertir a COP");
+                    valoraconvertir = teclado.nextDouble();
+                    currency="COP";
+                    modenasapi = consulta.consultarAPI(currency);
+                    moneda = new Moneda(currency,Double.valueOf(modenasapi.conversion_rates().get(currency)));
+                    valorconvertido = moneda.ConvertirAOTRA( valoraconvertir, moneda.getValor());
+                    moneda.messagetootra(valoraconvertir,currency,valorconvertido);
                     break;
                 case 6:
                     System.out.println("Opcion 6");
+                    System.out.println("Ingrese el valor a convertir a COP");
+                    valoraconvertir = teclado.nextDouble();
+                    currency="COP";
+                    modenasapi = consulta.consultarAPI(currency);
+                    moneda = new Moneda(currency,Double.valueOf(modenasapi.conversion_rates().get(currency)));
+                    System.out.println("Conversion a Dolares "+moneda.ConvertirAUSD(valoraconvertir,modenasapi.conversion_rates().get(currency)));
+                    valorconvertido = moneda.ConvertirAUSD(valoraconvertir,moneda.ConvertirAOTRA( valoraconvertir, modenasapi.conversion_rates().get(currency)));
+                    moneda.messagetousd(valoraconvertir,currency,valorconvertido);
                     break;
                 case 7:
                     System.out.println("Opcion 7");
